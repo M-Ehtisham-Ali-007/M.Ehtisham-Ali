fa26433aa3fc226eafc6e0d7b2de6e0dcc7d6466
M-Ehtisham-Ali-007 <101349655+M-Ehtisham-Ali-007@users.noreply.github.com>
2022-03-27 21:57:20 +0500

    Create App.js

---
+++ Lab-Task LOGO/App.js
@@ -0,0 +1,9 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
+import ButtonStyle from "./Components/ButtonStyle";
+import Logo from "./Components/Logo";
+
+const flex = () => {
+  return <Logo></Logo>;
+};
+export default flex;
